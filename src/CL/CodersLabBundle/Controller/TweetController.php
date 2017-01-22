068545affb726272cafdc0f851d014f279af318f
MateoMat <m.maslank@interia.pl>
2017-01-22 11:19:54 +0100

    Form function create

--- src/CL/CodersLabBundle/Controller/TweetController.php
+++ src/CL/CodersLabBundle/Controller/TweetController.php
@@ -7,37 +7,59 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use CL\CodersLabBundle\Entity\tweet;
 use CL\CodersLabBundle\Repository\tweetRepository;
 
-class TweetController extends Controller
-{
+class TweetController extends Controller {
+
     /**
      * @Route("/create")
      */
-    public function createAction()
-    {
+    public function createAction() {
+
+
         return $this->render('CLCodersLabBundle:Tweet:create.html.twig', array(
-            // ...
+                        // ...
         ));
+    }
+
+    /*
+     * Form builder for new and create actions
+     */
+
+    private function createForm(tweet $tweetObj) {
+        
+        $url=$this->generateUrl('create');
+        
+        $form = $this->createFormBuilder($tweetObj)
+                ->setAction($url)
+                ->setMethod('POST')
+                ->add('name', 'text')
+                ->add('text', 'text')
+                ->add('save', 'submit', array('label' => 'Dodaj'))
+                ->getForm();
+        
+        return $form;
     }
 
     /**
      * @Route("/new")
      */
-    public function newAction()
-    {
+    public function newAction() {
+
+
+
+
         return $this->render('CLCodersLabBundle:Tweet:new.html.twig', array(
-            // ...
+                        // ...
         ));
     }
 
     /**
      * @Route("/showAll")
      */
-    public function showAllAction()
-    {
-        $tweets=$this->getDoctrine()->getRepository('CLCodersLabBundle:tweet')->findAll();
-        
+    public function showAllAction() {
+        $tweets = $this->getDoctrine()->getRepository('CLCodersLabBundle:tweet')->findAll();
+
         return $this->render('CLCodersLabBundle:Tweet:show_all.html.twig', array(
-            'tweets'=>$tweets
+                    'tweets' => $tweets
         ));
     }
 
